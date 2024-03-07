3ae1991bcc5885025282137dbb8ab98914a2a7e7
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-03-06 23:46:33 -0300

    test: Add initial test setup

---
+++ ProductCatalog.Integration.Tests/Setup/TestEnvironment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductCatalog.Integration.Tests.Setup
+{
+    [SetUpFixture]
+    public class TestEnvironment
+    {
+        public static WebApplicationFactory<Program> Factory;
+
+        [OneTimeSetUp] 
+        public void RunBeforeAnyTests()
+        {
+            Factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Tests.json")
+                .Build();
+
+                builder.UseConfiguration(configuration);
+            });
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests() 
+        { 
+            Factory.Dispose();
+        }
+    }
+}
---
+++ ProductCatalog.Integration.Tests/Specs/ProductTests.cs
@@ -0,0 +1,16 @@
+namespace ProductCatalog.Integration.Tests.Specs
+{
+    public class ProductTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
