fc853572e36ac5a640593f6ffdb7ccff2296e43a
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-03-09 22:51:57 -0300

    feat: Add category repository

--- ProductCatalog/Infra/Repositories/CategoryRepository.cs
+++ ProductCatalog/Infra/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using ProductCatalog.Entities;
 using ProductCatalog.Infra;
 
@@ -13,14 +14,31 @@ namespace ProductCatalog
             _context = context;
         }
 
-        public async Task CreateCategory(Category category)
+        public async Task Create(Category category)
         {
             await _context.Categories.InsertOneAsync(category);
         }
 
-        public async Task<IEnumerable<Category>> GetAllCategories()
+        public async Task<IEnumerable<Category>> GetAll()
         {
             return await _context.Categories.Find(c => true).ToListAsync();
+        }
+        public async Task<Category?> GetCategoryById(ObjectId id)
+        {
+            var filter = Builders<Category>
+                .Filter
+                .Eq(p => p.Id, id);
+
+            return await _context.Categories.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task Delete(ObjectId id)
+        {
+            var filter = Builders<Category>
+                .Filter
+                .Eq(p => p.Id, id);
+
+            await _context.Categories.DeleteOneAsync(filter);
         }
     }
 }
